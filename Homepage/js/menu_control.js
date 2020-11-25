792d8c35a963027f9c2cc3db75a091ad033eab5b
Tanate62070085 <62070085@it.kmitl.ac.th>
2020-11-25 09:20:29 +0700

    fix silde

--- Homepage/js/menu_control.js
+++ Homepage/js/menu_control.js
@@ -9,20 +9,20 @@ var menu_men = ["#m1","#m2","#m3","#m4","#m5","#m6","#m7","#m8","#m9","#m10","#m
 var main_menu_men = ["#m_m_1","#m_m_2","#m_m_3","#m_m_4","#m_m_5","#m_m_6","#m_m_7","#m_m_8","#m_m_9","#m_m_10","#m_m_11","#m_m_12"]
 function openedrun(num){
     if(num==1 && opened){
-changemenu('#forwomen','#forwomen_1')
-$("#forwomen_1_line").animate({width : "96.5px"})
+    changemenu('#forwomen','#forwomen_1')
+    $("#forwomen_1_line").animate({width : "96.5px"})
     cat("#newproduct")
-        opened = false
+    opened = false
     }else if(num==2 && opened_1){
-changemenu2('#m1','#m_m_1')
-$("#m_m_1_line").animate({width : "157.17px"})
-cat("#women")
-opened_1 = false
+    changemenu2('#m1','#m_m_1')
+    $("#m_m_1_line").animate({width : "157.17px"})
+    cat("#women")
+    opened_1 = false
     }else if(num==3 && opened_2){
-changemenu1('#w1','#w_m_1')
-$("#w_m_1_line").animate({width : "161.5px"})
-cat("#men")
-opened_2 = false
+    changemenu1('#w1','#w_m_1')
+    $("#w_m_1_line").animate({width : "161.5px"})
+    cat("#men")
+    opened_2 = false
     }
 }
 function changemenu(menu,main_menu){
